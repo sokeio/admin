8aa18ef48e685ed50b49e0287ab150d0f8ec501f
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-12-02 08:50:41 +0700

    update

--- src/Livewire/SeoBox.php
+++ src/Livewire/SeoBox.php
@@ -25,7 +25,7 @@ class SeoBox extends Component
             $component->showMessage($manager->getMessage());
             // $component->closeComponent();
             $component->refreshRefComponent();
-        })->Message(function(){
+        })->Message(function () {
             return "seo update successfully";
         });
     }
