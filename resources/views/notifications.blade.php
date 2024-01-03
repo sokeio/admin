1eaa18005b353abdbf59e43478fd00eb018a82f9
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-01-04 01:07:34 +0700

    update

--- resources/views/notifications.blade.php
+++ resources/views/notifications.blade.php
@@ -13,7 +13,35 @@
         </a>
     @else
         <div wire:ignore class="offcanvas offcanvas-end" tabindex="-1" id="noticationUserManager"
-            aria-labelledby="offcanvasEndLabel" aria-modal="true" role="dialog">
+            aria-labelledby="offcanvasEndLabel" aria-modal="true" role="dialog" x-data="{
+                pageHtml: [],
+                getPageHtml() {
+                    return this.pageHtml.join('');
+                },
+                flgLoadMore: false,
+                async loadMore() {
+                    this.flgLoadMore = true;
+                    let html = await $wire.loadMore();
+                    if (html) {
+                        this.pageHtml.push(html);
+                    }
+                    this.flgLoadMore = false;
+                },
+                initNotification() {
+                    const divElement = $refs.notificationBody;
+                    let self = this;
+                    divElement.addEventListener('scroll', function() {
+                        const scrollPosition = divElement.scrollTop + divElement.clientHeight+300;
+                        const totalHeight = divElement.scrollHeight;
+            
+                        if (scrollPosition >= totalHeight) {
+                            self.loadMore();
+                        }
+                    });
+                    this.loadMore();
+                }
+            }"
+            x-init="initNotification">
             <div class="offcanvas-header bg-blue text--bold text-bg-blue p-2">
                 <h2 class="offcanvas-title" id="offcanvasEndLabel">@lang('Notication')</h2>
                 <button type="button" class="btn-close text-reset bg-white p-2 me-1" data-bs-dismiss="offcanvas"
@@ -29,269 +57,16 @@
                         </select>
                     </div>
                     <div class="col-auto ms-auto">
-                        <a href="#" class=" btn btn-secondary">
+                        <a href="#" wire:click='testData()' class=" btn btn-secondary">
                             Read All
-                          </a>
+                        </a>
                     </div>
                 </div>
+                <div x-show="loadMore" x-text="loadMore ? 'Loading...' : ''" x-cloak></div>
             </div>
-            <div class="offcanvas-body p-2">
-                <div class="divide-y">
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar">JL</span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    <strong>Jeffie Lewzey</strong> commented on your <strong>"I'm not a witch."</strong>
-                                    post.
-                                </div>
-                                <div class="text-secondary">yesterday</div>
-                            </div>
-                            <div class="col-auto align-self-center">
-                                <div class="badge bg-primary"></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar" style="background-image: url(./static/avatars/002m.jpg)"></span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    It's <strong>Mallory Hulme</strong>'s birthday. Wish him well!
-                                </div>
-                                <div class="text-secondary">2 days ago</div>
-                            </div>
-                            <div class="col-auto align-self-center">
-                                <div class="badge bg-primary"></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar" style="background-image: url(./static/avatars/003m.jpg)"></span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    <strong>Dunn Slane</strong> posted <strong>"Well, what do you want?"</strong>.
-                                </div>
-                                <div class="text-secondary">today</div>
-                            </div>
-                            <div class="col-auto align-self-center">
-                                <div class="badge bg-primary"></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar" style="background-image: url(./static/avatars/000f.jpg)"></span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    <strong>Emmy Levet</strong> created a new project <strong>Morning alarm
-                                        clock</strong>.
-                                </div>
-                                <div class="text-secondary">4 days ago</div>
-                            </div>
-                            <div class="col-auto align-self-center">
-                                <div class="badge bg-primary"></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar" style="background-image: url(./static/avatars/001f.jpg)"></span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    <strong>Maryjo Lebarree</strong> liked your photo.
-                                </div>
-                                <div class="text-secondary">2 days ago</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar">EP</span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    <strong>Egan Poetz</strong> registered new client as <strong>Trilia</strong>.
-                                </div>
-                                <div class="text-secondary">yesterday</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar" style="background-image: url(./static/avatars/002f.jpg)"></span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    <strong>Kellie Skingley</strong> closed a new deal on project <strong>Pen Pineapple
-                                        Apple Pen</strong>.
-                                </div>
-                                <div class="text-secondary">2 days ago</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar" style="background-image: url(./static/avatars/003f.jpg)"></span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    <strong>Christabel Charlwood</strong> created a new project for
-                                    <strong>Wikibox</strong>.
-                                </div>
-                                <div class="text-secondary">4 days ago</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar">HS</span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    <strong>Haskel Shelper</strong> change status of <strong>Tabler Icons</strong> from
-                                    <strong>open</strong> to <strong>closed</strong>.
-                                </div>
-                                <div class="text-secondary">today</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar" style="background-image: url(./static/avatars/006m.jpg)"></span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    <strong>Lorry Mion</strong> liked <strong>Tabler UI Kit</strong>.
-                                </div>
-                                <div class="text-secondary">yesterday</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar" style="background-image: url(./static/avatars/004f.jpg)"></span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    <strong>Leesa Beaty</strong> posted new video.
-                                </div>
-                                <div class="text-secondary">2 days ago</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar" style="background-image: url(./static/avatars/007m.jpg)"></span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    <strong>Perren Keemar</strong> and 3 others followed you.
-                                </div>
-                                <div class="text-secondary">2 days ago</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar">SA</span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    <strong>Sunny Airey</strong> upload 3 new photos to category
-                                    <strong>Inspirations</strong>.
-                                </div>
-                                <div class="text-secondary">2 days ago</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar" style="background-image: url(./static/avatars/009m.jpg)"></span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    <strong>Geoffry Flaunders</strong> made a <strong>$10</strong> donation.
-                                </div>
-                                <div class="text-secondary">2 days ago</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar" style="background-image: url(./static/avatars/010m.jpg)"></span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    <strong>Thatcher Keel</strong> created a profile.
-                                </div>
-                                <div class="text-secondary">3 days ago</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar" style="background-image: url(./static/avatars/005f.jpg)"></span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    <strong>Dyann Escala</strong> hosted the event <strong>Tabler UI Birthday</strong>.
-                                </div>
-                                <div class="text-secondary">4 days ago</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar" style="background-image: url(./static/avatars/006f.jpg)"></span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    <strong>Avivah Mugleston</strong> mentioned you on <strong>Best of 2020</strong>.
-                                </div>
-                                <div class="text-secondary">2 days ago</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="row">
-                            <div class="col-auto">
-                                <span class="avatar">AA</span>
-                            </div>
-                            <div class="col">
-                                <div class="text-truncate">
-                                    <strong>Arlie Armstead</strong> sent a Review Request to <strong>Amanda
-                                        Blake</strong>.
-                                </div>
-                                <div class="text-secondary">2 days ago</div>
-                            </div>
-                        </div>
-                    </div>
+            <div class="offcanvas-body p-2" x-ref="notificationBody">
+                <div class="divide-y" x-html="getPageHtml()">
                 </div>
-
             </div>
         </div>
     @endif
--- src/Livewire/Notifications.php
+++ src/Livewire/Notifications.php
@@ -3,7 +3,6 @@
 namespace Sokeio\Admin\Livewire;
 
 use Sokeio\Component;
-use Sokeio\Models\Notification;
 use Livewire\Attributes\On;
 use Livewire\Attributes\Reactive;
 
@@ -11,6 +10,7 @@ class Notifications extends Component
 {
     public $title = 'Last updates';
     public $notifications = [];
+    public $page = 1;
     public $showNewNotification = false;
     #[Reactive]
     public $showIcon = true;
@@ -21,11 +21,28 @@ class Notifications extends Component
     }
     public function mount()
     {
-        $this->loadNoiti();
     }
-    public function loadNoiti()
+    public function testData()
     {
-        $this->notifications = Notification::latest()->limit(10)->get()->toArray();
+        notification()->Title('test')->send();
+        notification()->Title('test2')->send();
+        notification()->Title('test3')->send();
+        notification()->Title('test4')->send();
+        notification()->Title('test5')->send();
+        notification()->Title('test5')->send();
+        notification()->Title('test6')->send();
+        notification()->Title('test7')->send();
+        notification()->Title('test8')->send();
+        $this->skipRender();
+    }
+    public function loadMore()
+    {
+
+
+
+        $this->skipRender();
+        $this->page = $this->page + 1;
+        return notification()->Render($this->page - 1);
     }
     public function render()
     {
