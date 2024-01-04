1ae8d249e3971adb5944073e6420fe9bed57c8d0
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-01-04 23:12:34 +0700

    update

--- themes/admin/resources/views/share/header.blade.php
+++ themes/admin/resources/views/share/header.blade.php
@@ -50,7 +50,19 @@
                     </svg>
                 </a>
                 <div class="nav-item dropdown d-none d-md-flex me-3">
-                    <livewire:admin::notifications>
+                    <a data-bs-toggle="offcanvas" href="#noticationUserManager" role="button"
+                        aria-controls="noticationUserManager" title="@lang('Show notifications')">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path
+                                d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6">
+                            </path>
+                            <path d="M9 17v1a3 3 0 0 0 6 0v-1"></path>
+                        </svg>
+                        <span class="badge bg-red"></span>
+                    </a>
                 </div>
             </div>
             @if (auth()->check())
@@ -78,4 +90,4 @@
 
     </div>
 </header>
-<livewire:admin::notifications :showIcon="false">
+<livewire:sokeio::notifications :showIcon="false">
