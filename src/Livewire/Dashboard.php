a85487e881ddb5fe52343d3c0745da6df62753a4
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-01-30 01:16:01 +0700

    Update Dashboard.php

--- src/Livewire/Dashboard.php
+++ src/Livewire/Dashboard.php
@@ -3,9 +3,14 @@
 namespace Sokeio\Admin\Livewire;
 
 use Sokeio\Component;
+use Sokeio\Facades\Assets;
 
 class Dashboard extends Component
 {
+    public function mount()
+    {
+        Assets::setTitle(__('Dashboard'));
+    }
     public function render()
     {
         return view('admin::dashboard.index', [
